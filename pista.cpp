8fd7dcced81a80cc05dac13968e8e2cd585a5383
Presidente100 <50965035+Presidente100@users.noreply.github.com>
2019-05-25 18:00:39 -0300

    Add files via upload

---
+++ pista.cpp
@@ -0,0 +1,86 @@
+/*
+	Nome: Cleber da Silva Melo
+	R.A.: 335338215650
+	2º Semestre Ciência da Computação
+*/
+
+/*
+	Jogo Enduro - Projeto de Final de Semestre
+*/
+
+
+/* Bibliotecas */
+#include <stdio.h>
+
+#include "tela.h"
+#include "carro.h"
+#include "pista.h"
+
+
+/* Constantes */
+
+
+/* Variáveis Globais */
+	unsigned char reta[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS];
+	unsigned char curva_direita[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS];
+	unsigned char curva_esquerda[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS];
+	//unsigned char pista_a_ser_exibida[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS];
+
+
+void Cria_Pistas()
+{
+	Cria_Pista_Reta();
+}
+
+
+
+void Cria_Pista_Reta()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	int centro_tela = TAMANHO_TELA_COLUNAS / 2;
+	
+	/* Inicializa a Matriz */
+	for(linha = 0; linha < TAMANHO_PISTA_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_PISTA_COLUNAS; coluna++)
+		{
+			reta[linha][coluna] = 0;
+		}
+	}
+	
+	
+	/* Preenche a Matriz com a Pista */
+	for(linha = 0; linha < TAMANHO_PISTA_LINHAS; linha++)
+	{
+		/* Borda Esquerda da Pista */
+		reta[linha][centro_tela - (linha)]		= 177;
+		reta[linha][centro_tela - (linha + 1)]	= 219;
+		
+		/* Borda Direita da Pista */
+		reta[linha][centro_tela + (linha + 1)]	= 177;
+		reta[linha][centro_tela + (linha + 2)]	= 219;
+	}
+}
+
+
+
+void Atualiza_Pista(unsigned char pista[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS])
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	
+	for(linha = 0; linha < TAMANHO_PISTA_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_PISTA_COLUNAS; coluna++)
+		{
+			pista[linha][coluna] = reta[linha][coluna];
+		}
+	}
+}
+
+
+
+
---
+++ pista.h
@@ -0,0 +1,32 @@
+/*
+	Nome: Cleber da Silva Melo
+	R.A.: 335338215650
+	2º Semestre Ciência da Computação
+*/
+
+/*
+	Jogo Enduro - Projeto de Final de Semestre
+*/
+
+#ifndef __PISTA__
+
+	/* Constantes */
+	#define __PISTA__
+		
+	#define TAMANHO_PISTA_LINHAS  (TAMANHO_TELA_LINHAS - (2 * ESPESSURA_BORDA_TELA))
+	#define TAMANHO_PISTA_COLUNAS (TAMANHO_TELA_COLUNAS - (2 * ESPESSURA_BORDA_TELA)) //(TAMANHO_CARRO_COLUNAS * 3)
+	/* Bibliotecas */	
+
+	/* Estruturas */
+	
+	/* Funções */
+	void Cria_Pistas();
+	void Cria_Pista_Reta();
+	void Atualiza_Pista(unsigned char pista[TAMANHO_PISTA_LINHAS][TAMANHO_PISTA_COLUNAS]);
+	
+#endif
+
+
+
+
+
