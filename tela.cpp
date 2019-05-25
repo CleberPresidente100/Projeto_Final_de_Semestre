21ec7597061ba739179855d330540cb643bfa5b2
Presidente100 <50965035+Presidente100@users.noreply.github.com>
2019-05-24 21:26:21 -0300

    Add files via upload

--- tela.cpp
+++ tela.cpp
@@ -23,28 +23,117 @@
 # define POSICAO_PLACAR_DIGITO_2 5
 # define POSICAO_PLACAR_DIGITO_3 10
 
+const unsigned char CARRO[TAMANHO_CARRO_LINHAS][TAMANHO_CARRO_COLUNAS] ={
+																			{221, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 222},
+																			{223, 223, 223, 223, 219, 219, 219, 219, 219, 219, 219, 223, 223, 223, 223},
+																			{177, 177, 177, ' ', ' ', ' ', 219, 219, 219, ' ', ' ', ' ', 177, 177, 177},
+																			{177, 177, 177, 220, 219, 219, 219, 219, 219, 219, 219, 220, 177, 177, 177},
+																			{177, 177, 177, 223, 219, 219, 219, 219, 219, 219, 219, 223, 177, 177, 177},
+																			{177, 177, 177, ' ', 223, 223, 223, 223, 223, 223, 223, ' ', 177, 177, 177}
+																		};
+
 /* Estruturas */
+
+struct struct_Carro
+{
+	//int pos_top_left_linha; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo do Carro
+	//int pos_top_left_coluna; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo do Carro
+	
+	int posicao_atual_carro;
+	unsigned char carro[TAMANHO_CARRO_LINHAS][TAMANHO_CARRO_COLUNAS];
+} carro;
+	
 struct struct_Placar
 {
-	int pos_top_left_x; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo do Placar
-	int pos_top_left_y; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo do Placar
+	//int pos_top_left_linha; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo do Placar
+	//int pos_top_left_coluna; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo do Placar
+	
 	unsigned char placar[TAMANHO_PLACAR_LINHAS][TAMANHO_PLACAR_COLUNAS];
 } placar;
 
 struct struct_Tela
 {
-	int pos_top_left_x; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo do Carro
-	int pos_top_left_y; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo do Carro
-	struct_Carro carro;
-	struct_Placar placar;
-	unsigned char tela[TAMANHO_TELA_LINHAS][TAMANHO_TELA_COLUNAS];
+	int pos_top_left_linha; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo da Tela
+	int pos_top_left_coluna; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo da Tela
+		
+	unsigned char tela_atual;
+	unsigned char proxima_tela;
+	unsigned char tela[NUMERO_DE_TELAS][TAMANHO_TELA_LINHAS][TAMANHO_TELA_COLUNAS];
 } tela;
 
 
 /* Funções Privadas */
 
 
+/* GERAIS */
+void Realiza_Inicializacao_das_Estruturas()
+{
+	Cria_Carro();
+	Cria_Placar();
+	Alterar_Placar(0);
+	//Cria_Pistas();
+	Inicializa_Telas();
+	
+}
 
+
+
+/* CARRO */
+void Cria_Carro()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	char auxiliar[TAMANHO_CARRO_LINHAS][TAMANHO_CARRO_COLUNAS] ={
+																	{221, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 222},
+																	{223, 223, 223, 223, 219, 219, 219, 219, 219, 219, 219, 223, 223, 223, 223},
+																	{177, 177, 177, ' ', ' ', ' ', 219, 219, 219, ' ', ' ', ' ', 177, 177, 177},
+																	{177, 177, 177, 220, 219, 219, 219, 219, 219, 219, 219, 220, 177, 177, 177},
+																	{177, 177, 177, 223, 219, 219, 219, 219, 219, 219, 219, 223, 177, 177, 177},
+																	{177, 177, 177, ' ', 223, 223, 223, 223, 223, 223, 223, ' ', 177, 177, 177}
+																};
+
+	
+	/* Carrega o Carro para dentro da Matriz carro */
+	for(linha = 0; linha < TAMANHO_CARRO_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_CARRO_COLUNAS; coluna++)
+		{
+			carro.carro[linha][coluna] = auxiliar[linha][coluna];
+		}
+	}
+}
+
+
+
+void Move_Carro(int movimentar_carro)
+{
+	carro.posicao_atual_carro += movimentar_carro;
+}
+
+
+
+void Inserir_Carro_na_Tela()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	//int pos_top_left_linha = TAMANHO_TELA_LINHAS - ESPESSURA_BORDA_TELA - TAMANHO_CARRO_LINHAS;
+	//int pos_top_left_coluna = (TAMANHO_TELA_COLUNAS / 2) - (TAMANHO_CARRO_COLUNAS / 2);
+	
+		
+	for(linha = 0; linha < TAMANHO_CARRO_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_CARRO_COLUNAS; coluna++)
+		{
+			tela.tela[tela.proxima_tela][linha + POSICAO_CARRO_LINHA][coluna + POSICAO_CARRO_COLUNA + carro.posicao_atual_carro] = carro.carro[linha][coluna];
+		}
+	}	
+}
+
+
+
+/* PLACAR */
 void Cria_Placar()
 {
 	int linha = 0;
@@ -61,6 +150,11 @@ void Cria_Placar()
 																	};
 
 	
+	/* Define a Posição na Tela onde o Placar deve começar a ser desenhado */
+	//placar.pos_top_left_linha = ESPESSURA_BORDA_TELA + 1;
+	//placar.pos_top_left_coluna = ESPESSURA_BORDA_TELA + 1;
+	
+	/* Carrega o Placar para dentro da Matriz placar */
 	for(linha = 0; linha < TAMANHO_PLACAR_LINHAS; linha++)
 	{
 		for(coluna = 0; coluna < TAMANHO_PLACAR_COLUNAS; coluna++)
@@ -120,6 +214,25 @@ void Alterar_Placar(int pontuacao)
 	}
 }
 
+
+
+void Inserir_Placar_na_Tela()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+		
+	for(linha = 0; linha < TAMANHO_PLACAR_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_PLACAR_COLUNAS; coluna++)
+		{
+			tela.tela[tela.proxima_tela][linha + POSICAO_PLACAR_LINHA][coluna + POSICAO_PLACAR_COLUNA] = placar.placar[linha][coluna];
+		}
+	}	
+}
+
+
+
 void Exibe_Placar()
 {
 	int linha = 0;
@@ -139,40 +252,60 @@ void Exibe_Placar()
 
 
 
-void Inicializa_Tela()	
+/* TELA */
+void Inicializa_Telas()
+{
+	int indice = 0;
+	
+	/* Inicializa Variáveis */
+	tela.pos_top_left_linha = 0; // Coordenada Y (Linha) na Tela Final do Pixel Superior Esquerdo da Tela
+	tela.pos_top_left_coluna = 0; // Coordenada X (Coluna) na Tela Final do Pixel Superior Esquerdo da Tela
+		
+	tela.tela_atual = 0;
+	tela.proxima_tela = 1;
+	
+	for(indice = 0; indice < NUMERO_DE_TELAS; indice++)
+	{
+		Limpa_Tela(indice);
+	}	
+}
+
+
+
+void Limpa_Tela(unsigned char indice_tela)
 {
 	int linha = 0;
 	int coluna = 0;
-	
+		
 	
 	/* Preenche a Tela com o caracter Espaço (Deixa a Tela "Em Branco") */
-	for(linha = 0; linha < TAMANHO_PLACAR_LINHAS; linha++)
+	for(linha = 0; linha < TAMANHO_TELA_LINHAS; linha++)
 	{
-		for(coluna = 0; coluna < TAMANHO_PLACAR_COLUNAS; coluna++)
+		for(coluna = 0; coluna < TAMANHO_TELA_COLUNAS; coluna++)
 		{
-			tela.tela[linha][coluna] = ' ';
+			tela.tela[indice_tela][linha][coluna] = 32; // Caractere Espaço
 		}
 	}
 	
 	/* Coloca as Bordas Horizontais (Superior e Inferior) */
 	for(coluna = 0; coluna < TAMANHO_TELA_COLUNAS; coluna++)
 	{			
-		tela.tela[0][coluna] = 205;
-		tela.tela[TAMANHO_TELA_LINHAS - 1][coluna] = 205;
+		tela.tela[indice_tela][0][coluna] = 205;
+		tela.tela[indice_tela][TAMANHO_TELA_LINHAS - 1][coluna] = 205;
 	}
 	
 	/* Coloca as Bordas Verticais (Esquerda e Direita) */
 	for(linha = 0; linha < TAMANHO_TELA_LINHAS; linha++)
 	{			
-		tela.tela[linha][0] = 186;
-		tela.tela[linha][TAMANHO_TELA_COLUNAS - 1] = 186;
+		tela.tela[indice_tela][linha][0] = 186;
+		tela.tela[indice_tela][linha][TAMANHO_TELA_COLUNAS - 1] = 186;
 	}
 	
 	/* Coloca as Bordas nos Vértices */
-	tela.tela[0][0] = 201;
-	tela.tela[0][TAMANHO_TELA_COLUNAS - 1] = 187;
-	tela.tela[TAMANHO_TELA_LINHAS - 1][0] = 200;
-	tela.tela[TAMANHO_TELA_LINHAS - 1][TAMANHO_TELA_COLUNAS - 1] = 188;
+	tela.tela[indice_tela][0][0] = 201;
+	tela.tela[indice_tela][0][TAMANHO_TELA_COLUNAS - 1] = 187;
+	tela.tela[indice_tela][TAMANHO_TELA_LINHAS - 1][0] = 200;
+	tela.tela[indice_tela][TAMANHO_TELA_LINHAS - 1][TAMANHO_TELA_COLUNAS - 1] = 188;
 	
 }
 
@@ -188,17 +321,86 @@ void Exibe_Tela()
 	{
 		for(coluna = 0; coluna < TAMANHO_TELA_COLUNAS; coluna++)
 		{
-			if((tela.tela[linha][coluna] != ' ') && (tela.tela[linha][coluna] != 0))
+			if(tela.tela[TELA_AUXILIAR][linha][coluna] != 0)
 			{
 				gotoxy(linha, coluna);
-				printf("%c", tela.tela[linha][coluna]);
+				printf("%c", tela.tela[TELA_AUXILIAR][linha][coluna]);
 			}
-					
 		}
-		
-		printf("\n");
-	}	
+	}
 }
+
+
+
+void Atualizar_Tela()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	Limpa_Tela(tela.proxima_tela);
+	//Inserir_Pista_na_Tela();
+	Inserir_Placar_na_Tela();
+	Inserir_Carro_na_Tela();
+	Comparar_Tela_Atual_com_Proxima_Tela();
+	Exibe_Tela();
+	
+	/* Atualiza Índices */
+	tela.tela_atual		= (~tela.tela_atual) & 0x01;
+	tela.proxima_tela	= (~tela.proxima_tela) & 0x01;
+	
+}
+
+
+
+void Comparar_Tela_Atual_com_Proxima_Tela()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	
+	for(linha = 0; linha < TAMANHO_TELA_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_TELA_COLUNAS; coluna++)
+		{				
+			if	(tela.tela[tela.tela_atual][linha][coluna] != tela.tela[tela.proxima_tela][linha][coluna])
+			{
+				tela.tela[TELA_AUXILIAR][linha][coluna] = tela.tela[tela.proxima_tela][linha][coluna];
+			}
+			else
+			{
+				tela.tela[TELA_AUXILIAR][linha][coluna] = 0;
+			}
+		}
+	}
+	
+}
+
+
+
+
+/*
+void Montar_Proxima_Tela()
+{
+	int linha = 0;
+	int coluna = 0;
+	
+	
+	for(linha = 0; linha < TAMANHO_TELA_LINHAS; linha++)
+	{
+		for(coluna = 0; coluna < TAMANHO_TELA_COLUNAS; coluna++)
+		{
+		}
+	}
+	
+}
+*/
+
+
+
+
+
+
+
 
 
 
--- tela.h
+++ tela.h
@@ -15,21 +15,54 @@
 	/* Constantes */
 	#define __TELA__
 	
-	#define TAMANHO_TELA_LINHAS 29
-	#define TAMANHO_TELA_COLUNAS 80
+	/* TELA */
+		// Tela sendo Exibida Atualmente, Próxima Tela a ser Exibida, Tela Auxiliar para Comparação
+		// As Telas Atual e Próxima são dinâmicas e ocuparão as posições 0 e 1 do Vetor.
+		// Já a Tela Auxiliar será sempre Última posição do Vetor.
+		#define NUMERO_DE_TELAS 3
+		#define TELA_AUXILIAR (NUMERO_DE_TELAS - 1)
+		
+		#define ESPESSURA_BORDA_TELA 1
+		#define TAMANHO_TELA_LINHAS 30
+		#define TAMANHO_TELA_COLUNAS 80
 	
-	#define ESPESSURA_BORDA_PLACAR 1
-	#define TAMANHO_PLACAR_LINHAS 7
-	#define TAMANHO_PLACAR_COLUNAS 16
+	/* PLACAR */
+		#define ESPESSURA_BORDA_PLACAR 1
+		#define TAMANHO_PLACAR_LINHAS 7
+		#define TAMANHO_PLACAR_COLUNAS 16
+		#define POSICAO_PLACAR_LINHA ESPESSURA_BORDA_TELA + 1
+		#define POSICAO_PLACAR_COLUNA ESPESSURA_BORDA_TELA + 1
+	
+	/* CARRO */
+		#define POSICAO_CARRO_LINHA (TAMANHO_TELA_LINHAS - ESPESSURA_BORDA_TELA - TAMANHO_CARRO_LINHAS)
+		#define POSICAO_CARRO_COLUNA ((TAMANHO_TELA_COLUNAS / 2) - (TAMANHO_CARRO_COLUNAS / 2))
+	
+	/* PISTA */
+		#define TAMANHO_PISTA (TAMANHO_CARRO_COLUNAS * 3)
 	
 	/* Estruturas */
 	
 	/* Funções */
-	void Cria_Placar();
-	void Exibe_Placar();
-	void Alterar_Placar(int pontuacao);
-	void Inicializa_Tela();
-	void Exibe_Tela();
+		/* Gerais */
+		void Realiza_Inicializacao_das_Estruturas();
+	
+		/* Placar */
+		void Cria_Placar();
+		void Exibe_Placar();
+		void Inserir_Placar_na_Tela();
+		void Alterar_Placar(int pontuacao);
+	
+		/* Tela */
+		void Inicializa_Telas();
+		void Limpa_Tela(unsigned char indice_tela);
+		void Exibe_Tela();
+		void Atualizar_Tela();
+		void Comparar_Tela_Atual_com_Proxima_Tela();
+	
+		/* Carro */
+		void Cria_Carro();
+		void Inserir_Carro_na_Tela();
+		void Move_Carro(int movimentar_carro);
 	
 #endif
 
